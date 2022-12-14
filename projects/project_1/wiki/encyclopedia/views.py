988e6c6fb06186660c80e0def6b4e3937b5e56c8
Hao Tang <61295938+haotangphuoc@users.noreply.github.com>
2022-12-14 01:31:55 -0400

    Initial commit

---
+++ projects/project_1/wiki/encyclopedia/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+from . import util
+
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+    
+def entry(request, title):
+    
+    
+    
+    
+    return entry
+
